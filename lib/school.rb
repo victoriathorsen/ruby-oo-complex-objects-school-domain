cfab62214fe98b2870d3d13e0500ca7db06edf23
Victoria Thorsen <victoriathorsenn@gmail.com>
2020-12-15 23:22:01 -0800

    commit tree 8c372e6b12546cf06c0a37726f3468e388ecfd37

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+
+class School
+
+    attr_accessor :student, :roster
+
+    def initialize (student)
+        @student = student
+        @roster = {}
+    end
+
+    def add_student (name, grade)
+        if @roster[grade]
+            @roster[grade] << name
+        else
+            @roster[grade] = [name]
+        end
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_hash = {}
+        @roster.sort.each do |grade, name|
+            sorted_hash[grade] = (name.sort)
+        end
+        sorted_hash
+    end
+
+
+
+end
